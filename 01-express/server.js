b4586c0218ec157935bd9d204d0a7b01575f0282
Chaofan Zhang <chaofan.zhang@ef.com>
2017-09-22 14:51:02 +0800

    01-express:
    Middleware:
    1. app.use(), nested middleware;
    2. serve static files by app.use(express.static(dirname));

--- 01-express/server.js
+++ 01-express/server.js
@@ -5,7 +5,25 @@ var express = require("express");
 
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
+app.use((request, response, next) => {
+    console.log("In middleware 1");
+    response.write("HEADER\n");
+    next();
+    console.log("Out of middleware 1");
+});
+
+
+app.use((request, response, next) => {
+    console.log("In middleware 2");
+    response.write("Header 2\n");
+    next();
+    console.log("Out of middleware 2、\n");
+});
+
 app.get("/", (request, response) => {
+    console.log("In handler");
     response.end("Hello World!");
 });
 
