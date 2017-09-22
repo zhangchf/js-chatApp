6c70a2a7d8caf95307f19a10a6f377ccd1c38020
Chaofan Zhang <chaofan.zhang@ef.com>
2017-09-22 15:33:25 +0800

    01-express:
    Use Express template engine Pug.
      npm install pug --save
      app.set("view engine", "pug");
      app.set("views", "./views");
      response.render(templatename, objectparam);

--- 01-express/server.js
+++ 01-express/server.js
@@ -5,11 +5,14 @@ var express = require("express");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "./views"); // this is unnecessary when you use the direct views subfolder.
+
 app.use(express.static(__dirname + "/public"));
 
 app.use((request, response, next) => {
     console.log("In middleware 1");
-    response.write("HEADER\n");
+    // response.write("HEADER\n");
     next();
     console.log("Out of middleware 1");
 });
@@ -17,14 +20,18 @@ app.use((request, response, next) => {
 
 app.use((request, response, next) => {
     console.log("In middleware 2");
-    response.write("Header 2\n");
+    // response.write("Header 2\n");
     next();
-    console.log("Out of middleware 2、\n");
+    console.log("Out of middleware 2");
 });
 
 app.get("/", (request, response) => {
     console.log("In handler");
     response.end("Hello World!");
+});
+
+app.get("/home", (request, response) => {
+    response.render("index", {title: "Home"});
 });
 
 const server = http.Server(app);
