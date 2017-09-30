5b2ccf325507108c220930b228e0a32e61d37c89
Chaofan Zhang <chaofan.zhang@ef.com>
2017-09-30 15:38:47 +0800

    02-app: Setup server skeleton: 1. install express, pug, socket.io. 2. build up basic server config, and index.pug.

--- 02-app/src/server/server.js
+++ 02-app/src/server/server.js
@@ -2,7 +2,51 @@
 // include source map support
 import "source-map-support/register";
 
-import {blegh} from "shared/test";
+import express from "express";
+import http from "http";
+import socketIo from "socket.io";
 
-blegh();
-console.log("Hi");
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+// ---------------
+// Setup
+const app = express();
+const server = new http.Server(app);
+const io = socketIo(server);
+
+// ---------------
+// Client Webpack
+
+// ---------------
+// Configure Express
+app.set("view engine", "pug");
+app.use(express.static("public"));
+
+const useExternalStyles = !isDevelopment;
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        useExternalStyles
+    });
+});
+
+// ---------------
+// Modules
+
+// ---------------
+// Socket
+io.on("connection", socket => {
+    console.log(`Got connection from ${socket.request.connection.remoteAddress}`);
+});
+
+// ---------------
+// Startup
+const port = process.env.PORT || 3000;
+function startServer() {
+    server.listen(port, () => {
+        console.log(`Started http server on ${port}`);
+    });
+}
+
+startServer();
+
